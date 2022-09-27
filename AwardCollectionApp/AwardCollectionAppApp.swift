7fe57c23a46889246a57adfb9cd6b47efa6030b4
Vadim Gamzaev <Vadosbox@gmail.com>
2022-09-27 17:44:51 +0300

    Initial Commit

---
+++ AwardCollectionApp/AwardCollectionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwardCollectionAppApp.swift
+//  AwardCollectionApp
+//
+//  Created by Вадим Гамзаев on 27.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct AwardCollectionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
